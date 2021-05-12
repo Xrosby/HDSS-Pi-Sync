cad0ff2a51281e501f25cfdf4f226dfb097aaec8
Lasse <lklev16@student.sdu.dk>
2021-05-12 14:59:37 +0200

    endpoints for shutting down and restarting

--- raspberry-sync/__init__.py
+++ raspberry-sync/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from data_sync import data_sync, sync_main_server
+import subprocess
 
 app = Flask(__name__)
 
@@ -9,6 +10,11 @@ def sync():
     tablet_id = request.args.get("tablet_id")
     dump = request.data
     return data_sync(tablet_id, dump)
+
+@app.route("/sync_with_dk", methods=["POST"])
+def sync_with_dk():
+    token = request.json.get("jwt")
+    return sync_main_server(token)
 
 
 @app.route("/get_dump")
@@ -23,10 +29,20 @@ def ping():
     return "Ping!"
 
 
-@app.route("/sync_with_dk", methods=["POST"])
-def sync_with_dk():
-    token = request.json.get("jwt")
-    return sync_main_server(token)
+@app.route("/restart", methods=["GET"])
+def restart():
+    subprocess.call("sudo reboot", shell=True)
+    return
+
+@app.route("/shutdown", methods=["GET"])
+def shutdown():
+    subprocess.call("sudo shutdown now", shell=True)
+    return
+
+
+
+
+
 
 
 if __name__ == "__main__":
